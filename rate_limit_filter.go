5371defc025ab7a9480999f5ec22e25ae8e200ca
Joël Stemmer <stemmertech@gmail.com>
2015-01-25 19:07:22 +0100

    Create rate limit filter
    Fixes #2

---
+++ rate_limit_filter.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"log"
+	"strconv"
+	"time"
+
+	"github.com/boltdb/bolt"
+)
+
+func init() {
+	RegisterComponent("rate-limit-filter", RateLimitFilter{})
+}
+
+// RateLimitFilter limits the number of requests within a certain time interval.
+type RateLimitFilter struct{}
+
+// Name returns the name of this component.
+func (RateLimitFilter) Name() string { return "Rate limiter" }
+
+// Template returns the HTML template name of this component.
+func (RateLimitFilter) Template() string { return "rate-limit-filter" }
+
+// Init initializes this component. It requires an amount and an interval in
+// seconds to be present.
+func (RateLimitFilter) Init(h Hook, params map[string]string, b *bolt.Bucket) error {
+	amount, ok := params["amount"]
+	if !ok {
+		return errors.New("amount is required")
+	}
+
+	if i, err := strconv.Atoi(amount); err != nil || i <= 0 {
+		return fmt.Errorf("amount must be a positive number > 0: %s", err)
+	}
+
+	interval, ok := params["interval"]
+	if !ok {
+		return errors.New("interval is required")
+	}
+
+	if i, err := strconv.Atoi(interval); err != nil || i <= 0 {
+		return fmt.Errorf("interval must be a positive number: %s", err)
+	}
+
+	if err := b.Put([]byte(fmt.Sprintf("%s-amount", h.ID)), []byte(amount)); err != nil {
+		return err
+	}
+	if err := b.Put([]byte(fmt.Sprintf("%s-interval", h.ID)), []byte(interval)); err != nil {
+		return err
+	}
+	_, err := b.CreateBucketIfNotExists([]byte("requests"))
+	return err
+}
+
+// Process makes sure incoming requests do not exceed the configured rate
+// limit.
+func (RateLimitFilter) Process(h Hook, r Request, b *bolt.Bucket) error {
+	amount, _ := strconv.Atoi(string(b.Get([]byte(fmt.Sprintf("%s-amount", h.ID)))))
+	interval, _ := strconv.Atoi(string(b.Get([]byte(fmt.Sprintf("%s-interval", h.ID)))))
+	if amount <= 0 || interval <= 0 {
+		return errors.New("rate limit filter not initialized")
+	}
+
+	b = b.Bucket([]byte("requests"))
+
+	// store current timestamp
+	now := time.Now()
+	k := []byte(fmt.Sprintf("%d", now.UnixNano()))
+	log.Printf("storing %s", string(k))
+	if err := b.Put(k, nil); err != nil {
+		return err
+	}
+
+	// count requests
+	c := b.Cursor()
+	from := []byte(fmt.Sprintf("%d", now.Add(time.Duration(-interval)*time.Second).UnixNano()))
+
+	log.Printf("from: %s", string(from))
+
+	var count int
+	for k, _ := c.Seek(from); k != nil; k, _ = c.Next() {
+		log.Printf("counting %s", string(k))
+		count++
+	}
+
+	if count > amount {
+		return fmt.Errorf("rate limit exceeded (limit=%d count=%d)", amount, count)
+	}
+
+	// cleanup old entries
+	for k, _ := c.First(); k != nil && bytes.Compare(k, from) <= 0; k, _ = c.Next() {
+		log.Printf("deleting %s", string(k))
+		if err := b.Delete(k); err != nil {
+			return err
+		}
+	}
+	return nil
+}
